98c317f5035ff78ef85c97224934fea504761a07
nicomech <facebook@mechin.org>
2016-01-24 22:16:39 +0100

    test

---
+++ loginTest.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.copro-eco.fr/")
+elem = driver.find_element_by_name("login")
+elem.send_keys("copro@mechin.org")
+
+elem = driver.find_element_by_name("pwd")
+elem.send_keys("azerty")
+
+elem.submit()
+
+driver.close()
