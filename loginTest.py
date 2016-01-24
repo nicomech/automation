515cbdfad7f250e6f18bc36546056791e1d69c1b
nicomech <facebook@mechin.org>
2016-01-24 23:30:27 +0100

    phantimjs test

--- loginTest.py
+++ loginTest.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 #driver = webdriver.Firefox()
-driver = webdriver.PhantomJS('./phantomjs')
+driver = webdriver.PhantomJS()
 driver.get("http://www.copro-eco.fr/")
 elem = driver.find_element_by_name("login")
 elem.send_keys("copro@mechin.org")
