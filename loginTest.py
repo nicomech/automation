5311ebb55d424c88d2bd1fa81d4f2566e94909f4
nicomech <facebook@mechin.org>
2016-01-25 15:20:27 +0100

    ubuntu home

--- loginTest.py
+++ loginTest.py
@@ -3,15 +3,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+#Stockholm
+#myUrl = "https://87.60.87.90"
+#Finland
+#myUrl = "https://194.111.72.47"
+#test
+myUrl = "https://www.ebay.com"
 driver = webdriver.Firefox()
 #driver = webdriver.PhantomJS()
-driver.get("http://www.copro-eco.fr/")
-elem = driver.find_element_by_name("login")
-elem.send_keys("copro@mechin.org")
+driver.get(myUrl)
+#elem = driver.find_element_by_name("login")
+#elem.send_keys("copro@mechin.org")
 
-elem = driver.find_element_by_name("pwd")
-elem.send_keys("azerty")
+#elem = driver.find_element_by_name("pwd")
+#elem.send_keys("azerty")
 
-elem.submit()
+#elem.submit()
 
 driver.close()
